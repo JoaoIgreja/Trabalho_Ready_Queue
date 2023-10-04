30ca86224cbec99ba370f2eb1fc011a67fe704ee
JoaoIgreja <jvigreja@gmail.com>
2023-10-04 12:31:52 -0300

    Commit

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class Processo:
 def obter_prioridade(processo):
     return processo.prioridade
 
+def obter_tempo_execucao(processo):
+    return processo.tempo_execucao
+
 if __name__ == "__main__":
     processos = [
         Processo("P1", 2, 5),
@@ -17,21 +20,41 @@ if __name__ == "__main__":
         Processo("P6", 4, 16),
     ]
 
-    print("Simulador de ready queue (PS):")
+    print("\nSimulador de ready queue (PS):")
 
     processos.sort(key=obter_prioridade)
 
-    tempo_total = 0
+    print("Prioridade:")
+    tempo_total_prioridade = 0
 
     for processo in processos:
-        if (processo.tempo_execucao == 1):
+        if processo.tempo_execucao == 1:
             print(f" {processo.nome} Está Pronto para ser Executado (Prioridade {processo.prioridade}) utilizando {processo.tempo_execucao} unidade de tempo")
-            tempo_total += processo.tempo_execucao -1
+            tempo_total_prioridade += processo.tempo_execucao - 1
         else:
             print(f" {processo.nome} Está Pronto para ser Executado (Prioridade {processo.prioridade}) utilizando {processo.tempo_execucao} unidades de tempo")
-        tempo_total += processo.tempo_execucao
+        tempo_total_prioridade += processo.tempo_execucao
 
-    if (tempo_total != 1):
-        print(f"Tempo total de execução: {tempo_total} unidades de tempo.")
+    if tempo_total_prioridade != 1:
+        print(f"{tempo_total_prioridade} unidades de tempo.")
     else:
-         print(f"Tempo total de execução: {tempo_total} unidade de tempo.")
+         print(f"{tempo_total_prioridade} unidade de tempo.")
+
+   
+    processos.sort(key=obter_tempo_execucao)
+
+    print("\nSJF:")
+    tempo_total_sjf = 0
+
+    for processo in processos:
+        if processo.tempo_execucao == 1:
+            print(f" {processo.nome} Está Pronto para ser Executado (Tempo de Execução {processo.tempo_execucao}) utilizando {processo.tempo_execucao} unidade de tempo")
+            tempo_total_sjf += processo.tempo_execucao - 1
+        else:
+            print(f" {processo.nome} Está Pronto para ser Executado (Tempo de Execução {processo.tempo_execucao}) utilizando {processo.tempo_execucao} unidades de tempo")
+        tempo_total_sjf += processo.tempo_execucao
+
+    if tempo_total_sjf != 1:
+        print(f"{tempo_total_sjf} unidades de tempo.")
+    else:
+         print(f"{tempo_total_sjf} unidade de tempo.\n")
